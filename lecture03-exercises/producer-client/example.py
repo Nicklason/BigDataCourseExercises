4f3788544bad14755bc0f8134a89f7fc0abfdd00
Nicklas Marc Pedersen <nicklas.marc.pedersen@gmail.com>
2022-09-20 16:13:55 +0200

    create producer

--- lecture03-exercises/producer-client/example.py
+++ lecture03-exercises/producer-client/example.py
@@ -1,5 +1,11 @@
-# input
-string = str(input())
-  
-# output
-print(string)
+from kafka import KafkaProducer
+producer = KafkaProducer(bootstrap_servers=['kafka:9092'])
+
+string = ''
+while (string != 'exit'):
+    # input
+    string = str(input())
+
+    producer.send('foo', str.encode(string))
+
+producer.flush()
