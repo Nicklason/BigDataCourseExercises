fc2adfa777b9517c9cc83d9f84ac55917aa2d6b2
Nicklas Marc Pedersen <nicklas.marc.pedersen@gmail.com>
2022-09-27 15:55:55 +0200

    lecture 4 exercise 4

--- lecture04-exercises/sentimentExercise/example.py
+++ lecture04-exercises/sentimentExercise/example.py
@@ -15,22 +15,33 @@ conf = SparkConf().set('spark.executor.cores', 1).set('spark.cores.max',1).set('
 sc = SparkContext(master='spark://spark-master:7077', appName='myAppName', conf=conf)
 
 # TODO: HINT! Remove select_words and define a function to return an integer based on the sentiment of the input word
-select_words = lambda s : s[1] > 400
+#select_words = lambda s : s[1] > 400
 
-files = "hdfs://namenode:9000/stream-in/"
+def select_words(s):
+    if s in positiveWords:
+        return 1
+    elif s in negativeWords:
+        return -1
+    else:
+        return 0
+
+files = "hdfs://namenode:9000/txt/"
 # Read in all files in the directory
 txtFiles = sc.wholeTextFiles(files, 20)
 # Take the content of the files and split them
-all_word = txtFiles.flatMap(lambda s: s[1].split())
+all_word = txtFiles.flatMap(lambda s: s[1].lower().split())
 
 # TODO: HINT! Replace the implementation for word_map to run a function on each word, will return an RDD of just integers (basically a list of just integers)
 # Change from list of words to list of (word, 1)
-word_map = all_word.map(lambda s: (s, 1))
+word_map = all_word.map(lambda s: (s, select_words(s)))
 
 # TODO: HINT! Remove the rest of this code, and create and print the result by reducing the word_map with the operator "add"
 # Merge values with equal keys
 word_reduce = word_map.reduceByKey(lambda s, t: s+t)
+
+total = word_reduce.map(lambda s: s[1]).reduce(lambda s, t: s+t)
+
 # Filter using the defined lambda and sort by value
-top_words = word_reduce.filter(select_words).sortBy(lambda s: s[1])
+#top_words = word_reduce.filter(select_words).sortBy(lambda s: s[1])
 # Collect to a Python list and print
-print(top_words.collect())
+print(total)
